b9685043e76bde2cbfd317b4d68576340c0d9471
Kaushik Mandal <koushik369mondal@gmail.com>
2025-09-10 09:01:30 +0530

    Add Express server setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
 // const express = require('express');
 import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(5001, () => {
+    console.log('Server is running on PORT:5001');
+});
