4f023deffd362ee015254816b2cbf87f0d51cd47
Kaushik Mandal <koushik369mondal@gmail.com>
2025-09-23 23:23:22 +0530

    configure dotenv and update server port handling

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 // const express = require('express');
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.send('Hello from Kaushik');
 });
 
-app.listen(5001, () => {
-    console.log('Server is running on PORT:5001');
+console.log("My port:", PORT)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
 });
