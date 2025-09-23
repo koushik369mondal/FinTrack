9dabbeb34892dc5b5557ae542ac412045f643c0b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-09-23 23:50:02 +0530

    Add database initialization logic in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
+import { sql } from './config/db.js';
 
 dotenv.config();
 
@@ -8,14 +9,30 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// connectDB(process.env.DATABASE_URL);
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.error("Error initializing database:", error);
+        process.exit(1); // status code 1 means failure, 0 means success
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Hello from Kaushik');
 });
 
-console.log("My port:", PORT)
-
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT:${PORT}`);
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT}`);
+    });
 });
