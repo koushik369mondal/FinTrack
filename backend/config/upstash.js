51b66931e5c1b3d056815d6ebd4b0899c16b2b0b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-24 13:34:10 +0530

    implementation and rename variable for consistency

--- backend/config/upstash.js
+++ backend/config/upstash.js
@@ -3,9 +3,9 @@ import { Ratelimit } from "@upstash/ratelimit";
 
 import "dotenv/config";
 
-const ratelimiter = new Ratelimit({
+const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "60 s"),
 });
 
-export default ratelimiter;
+export default ratelimit;
