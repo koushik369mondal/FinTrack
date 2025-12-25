c0312a9641d8442a05952ab9d6a0a5cbd84a68dd
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-25 21:14:19 +0530

    move files to src directory and remove deprecated code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,9 @@
-import {neon} from"@neondatabase/serverless"
+import { neon } from "@neondatabase/serverless"
 
 import "dotenv/config";
 
 //Creates a SQL connecction using our DB URL
-export const  sql = neon(process.env.DATABASE_URL);
+export const sql = neon(process.env.DATABASE_URL);
 
 export async function initDB() {
     try {
