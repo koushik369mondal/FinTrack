9431e4f3710c9bfd5ba9a906aa7f63425b4cf45a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-25 18:22:10 +0530

    Fix import statement for database configuration and update transaction routes for consistency

--- backend/routes/transactions.route.js
+++ backend/routes/transactions.route.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import { sql } from '../config/db.js';
-import { createTransaction, deleteTransaction, getTransactionsByUserId } from '../controllers/transactions.controller.js';
+import { createTransaction, deleteTransaction, getTransactionsByUserId, getTransactionSummary } from '../controllers/transactions.controller.js';
 
 const router = express.Router();
 
-router.get("/:userId", getTransactionsByUserId);
-
 router.post("/", createTransaction);
 
-router.delete("/:id", deleteTransaction);
+router.get("/summary/:userId", getTransactionSummary);
 
-router.get("/summary/:userId", );
+router.get("/:userId", getTransactionsByUserId);
+
+router.delete("/:id", deleteTransaction);
 
 export default router;
