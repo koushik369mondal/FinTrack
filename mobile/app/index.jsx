8d1c4d9717d734c0a6cad373e9f47ccc667cbecf
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-26 12:22:32 +0530

    Update RootLayout to hide header and add image to Index screen

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,3 +1,4 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
@@ -12,6 +13,11 @@ export default function Index() {
     >
       <Text style={{ color: "black" }}>Edit app/index.tsx to edit this screen.</Text>
       <Link href="/about">About</Link>
+
+      <Image
+        source={{ uri: "https://images.unsplash.com/photo-1765211003001-b9eb5cbfe1f3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+        style={{ width: 200, height: 200, marginTop: 20 }}
+      />
     </View>
   );
 }
