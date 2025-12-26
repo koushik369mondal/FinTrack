d1d70cba5c2f861ef4949c3a26829d317c0439bc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-26 21:34:41 +0530

    simplify layout and remove unused styles

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -4,32 +4,8 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}>
-      <Text style={styles.heading}>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/about">About</Link>
-
-      <View>
-        Hello
-      </View>
-
-      <Image
-        source={{ uri: "https://images.unsplash.com/photo-1765211003001-b9eb5cbfe1f3?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
-        style={{ width: 200, height: 200, marginTop: 20 }}
-      />
+    <View>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  heading: {
-    fontSize: 20,
-    marginBottom: 20,
-    color: "blue",
-  },
-})
