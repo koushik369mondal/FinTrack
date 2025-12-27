23dab807e66faf65fb8d7c1c57c0d03820d25d0d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-27 17:14:39 +0530

    Update layout to hide header in RootLayout and apply styles to Index screen

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -4,8 +4,14 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "purple",
+  },
+});
