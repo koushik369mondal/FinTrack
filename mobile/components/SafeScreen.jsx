5e49507bd6640e8175d872e1299a73e226328ae8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-28 09:25:42 +0530

    Add @clerk/clerk-expo dependency to mobile package

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,12 +1,13 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { COLORS } from '@/constants/colors.js';
 
 const SafeScreen = ({ children }) => {
     const insets = useSafeAreaInsets();
+
     return (
-        <View style={{paddingTop:insets.top, flex:1, backgroundColor: COLORS.background}}>
+        <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background }}>
             {children}
         </View>
     )
