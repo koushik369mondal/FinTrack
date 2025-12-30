9fabbb791e46e63b10cf2194c8971879fa97a8c6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-30 14:59:05 +0530

    Refactor API_URL handling by moving it to a constants file for better organization

---
+++ mobile/constants/api.js
@@ -0,0 +1,2 @@
+// const API_URL = "http://localhost:5001/api";
+export const API_URL = "https://fintrack-backend-bhy0.onrender.com/api";
--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,9 +1,10 @@
 
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
+import { API_URL } from "../constants/api";
 
 // const API_URL = "http://localhost:5001/api";
-const API_URL = "https://fintrack-backend-bhy0.onrender.com/api";
+// const API_URL = "https://fintrack-backend-bhy0.onrender.com/api";
 
 export const useTransactions = (userId) => {
     const [transactions, setTransactions] = useState([]);
